dc51479603895a7e7346420f3f6ac74bb4965b3a
harvey_chen <harveychenwork@gmail.com>
2023-06-01 23:08:04 +1000

    add sql example

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const { readings } = require("./readings/readings");
 const { readingsData } = require("./readings/readings.data");
 const { read, store } = require("./readings/readings-controller");
 const { recommend, compare } = require("./price-plans/price-plans-controller");
+const knex = require('knex')(require('../knexfile').development);
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
@@ -24,6 +26,42 @@ app.get("/price-plans/recommend/:smartMeterId", (req, res) => {
 app.get("/price-plans/compare-all/:smartMeterId", (req, res) => {
     res.send(compare(getReadings, req));
 });
+
+
+//below for test purpose
+// Create a new user
+app.post('/users', (req, res) => {
+    knex('users').insert({
+      name: req.body.name,
+      email: req.body.email
+    }).then(() => res.status(201).send('User added'))
+      .catch(error => res.status(500).send(error));
+  });
+  
+  // Get all users
+  app.get('/users', (req, res) => {
+    knex('users').select().then(users => res.send(users));
+  });
+  
+  // Update an existing user
+  app.put('/users/:id', (req, res) => {
+    knex('users')
+      .where('id', req.params.id)
+      .update({
+        name: req.body.name,
+        email: req.body.email
+      }).then(() => res.send('User updated'))
+        .catch(error => res.status(500).send(error));
+  });
+  
+  // Delete a user
+  app.delete('/users/:id', (req, res) => {
+    knex('users')
+      .where('id', req.params.id)
+      .del().then(() => res.send('User deleted'))
+        .catch(error => res.status(500).send(error));
+  });
+  
 
 const port = process.env.PORT || 8080;
 app.listen(port);
