afd9333f05242ab5aa7c22c47d381a4fbb5dccfe
harvey_chen <harveychenwork@gmail.com>
2023-06-01 23:10:06 +1000

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,23 @@
 // Update with your config settings.
+//  npx knex migrate
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
 
+module.exports = {
   development: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'root',
-      database: 'power',
-      charset: 'utf8'
+      host: "127.0.0.1",
+      user: "root",
+      password: "root",
+      database: "power",
+      charset: "utf8",
     },
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
-    }
+      tableName: "knex_migrations",
+      directory: "./migrations",
+    },
   },
-
 };
